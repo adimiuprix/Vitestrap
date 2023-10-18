418dccc10b3795a92d0871e2abada5d7c82e27a9
Adi pranoto <miuprix@gmail.com>
2023-10-18 09:23:03 +0000

    bg header

--- src/components/Heaader.jsx
+++ src/components/Heaader.jsx
@@ -1,6 +1,6 @@
 function Header(){
     return(
-        <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 bg-green-400">
+        <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-4 bg-green-400">
             <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
             <div class="flex items-center justify-between">
                 <a class="flex-none text-xl font-semibold text-white" href="/">Staking demo</a>
