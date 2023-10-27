eaa9b5f1979e48958a91d625572f6bf18ce4b1aa
Adi pranoto <miuprix@gmail.com>
2023-10-27 03:12:22 +0000

    memisah tombol stake dan unstake

--- src/components/BalanceBox.jsx
+++ src/components/BalanceBox.jsx
@@ -1,3 +1,4 @@
+import { ActionBox } from './ActionBox'
 import { useEffect, useState } from 'react'
 import { useContractRead, useAccount } from 'wagmi'
 import { ContractStaking } from '../constant/ContractStaking'
@@ -38,14 +39,7 @@ function BalanceBox(){
                             <div className="text-lg sm:text-xl font-semibold text-green-400">Balance</div>
                             <p className="mt-2 sm:mt-3 text-2xl sm:text-2xl font-bold text-green-500">{isLoading ? 'Loading...' : balance ?? 'Data unavailable'} AGRA</p>
                         </div>
-                        <div className="text-center gap-5 pt-5">
-                            <button type="button" className="py-3 px-4 inline-flex justify-center items-center rounded-md bg-green-100 border border-transparent font-semibold text-green-500 hover:text-white hover:bg-teal-500 focus:outline-none focus:ring-2 ring-offset-white focus:ring-green-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800 m-3">
-                            Stake
-                            </button>
-                            <button type="button" className="py-3 px-4 inline-flex justify-center items-center rounded-md bg-green-100 border border-transparent font-semibold text-green-500 hover:text-white hover:bg-teal-500 focus:outline-none focus:ring-2 ring-offset-white focus:ring-green-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800 m-3">
-                            Unstake
-                            </button>
-                        </div>
+                        <ActionBox />
                     </div>
                 </div>
             </div>
